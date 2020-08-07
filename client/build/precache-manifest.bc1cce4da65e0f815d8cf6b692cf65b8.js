a7939b8013b5421583bc0c3f64a42cb6f70c8f12
Agbanusi <agbanusijohn@gmail.com>
2020-08-07 06:03:37 +0100

    him

---
+++ client/build/precache-manifest.bc1cce4da65e0f815d8cf6b692cf65b8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ea7d636953d767cbf1500bd9315fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e95ed0f6b60fa66a6af",
+    "url": "/static/css/main.a3195de0.chunk.css"
+  },
+  {
+    "revision": "f7149897165d10aa4b0f",
+    "url": "/static/js/2.9d4b8a8d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.9d4b8a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e95ed0f6b60fa66a6af",
+    "url": "/static/js/main.cb0cf61e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "33a2d2e6c53e83e1a22a90663dd871e9",
+    "url": "/static/media/2860933.33a2d2e6.jpg"
+  },
+  {
+    "revision": "1257a6156712fd19a5fbc7533816bcb3",
+    "url": "/static/media/Confetti-Doodles.1257a615.svg"
+  },
+  {
+    "revision": "f66c1ef50f54766c9627bad94e8fc94d",
+    "url": "/static/media/Diamond-Sunset.f66c1ef5.svg"
+  },
+  {
+    "revision": "4f96b4e8e1bc3ca37ea4d86e1bd01960",
+    "url": "/static/media/Liquid-Cheese.4f96b4e8.svg"
+  },
+  {
+    "revision": "4469b7bee9c23058318ca31ccf696cda",
+    "url": "/static/media/Rainbow-Vortex.4469b7be.svg"
+  },
+  {
+    "revision": "3eb74d6f0cf538d3668abb9483952e8b",
+    "url": "/static/media/Repeating-Triangles.3eb74d6f.svg"
+  },
+  {
+    "revision": "5afc248fc18706ccef776ce1fb117750",
+    "url": "/static/media/semi-opened-laptop-computer-turned-on-on-table-2047905.5afc248f.jpg"
+  },
+  {
+    "revision": "5d62a91ce1f7103ebc6bcc8f1185c418",
+    "url": "/static/media/transport-3366391_640.5d62a91c.jpg"
+  },
+  {
+    "revision": "7a14df2a71698f115fb91228441e9e5d",
+    "url": "/static/media/undraw_Online_page_re_lhgx.7a14df2a.svg"
+  },
+  {
+    "revision": "b0c11510c7d2a8e2f2402be014387b85",
+    "url": "/static/media/undraw_business_shop_qw5t.b0c11510.png"
+  },
+  {
+    "revision": "aefce4489355e8e0515c6e5248a511ec",
+    "url": "/static/media/undraw_gone_shopping_vwmc.aefce448.svg"
+  },
+  {
+    "revision": "d554b7d25ed280723289d2447e7e9fba",
+    "url": "/static/media/undraw_group_selfie_ijc6.d554b7d2.png"
+  },
+  {
+    "revision": "532cde0d5eb503556780ead07548859c",
+    "url": "/static/media/undraw_online_video_ivvq.532cde0d.svg"
+  },
+  {
+    "revision": "f73c5c4ed4959122fa7a514d79fc9e9c",
+    "url": "/static/media/undraw_selfie_time_cws4.f73c5c4e.png"
+  },
+  {
+    "revision": "d5872f4a3f4f6e3d606e536a87338dd7",
+    "url": "/static/media/undraw_web_search_eetr.d5872f4a.svg"
+  },
+  {
+    "revision": "bdca546f258d91bc24315a7856636349",
+    "url": "/static/media/white-headphones-3394650.bdca546f.jpg"
+  }
+]);
